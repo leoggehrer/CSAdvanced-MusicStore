515d5f8786cacc3a07adc26f9d075918ee2cb188
Gehrer Gerhard <g.gehrer@htl-leonding.ac.at>
2021-11-04 20:44:46 +0100

    Added print artist and albums

--- MusicStore.ConApp/Program.cs
+++ MusicStore.ConApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using MusicStore.Statistics;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MusicStore.ConApp
 {
@@ -13,6 +15,21 @@ namespace MusicStore.ConApp
 			var tracks = Loader.LoadTracks(@"Data\Track.csv");
 			var genres = Loader.LoadGenres(@"Data\Genre.csv");
 			var albumModels = Loader.CreateObjectModel(albums, artists, tracks, genres);
+
+			PrintArtistAndAlbums(albumModels);
+		}
+
+		static void PrintArtistAndAlbums(IEnumerable<Models.Album> albums)
+		{
+			var query = albums.GroupBy(al => al.Artist.Name, l => l.Title);
+
+			query.ToList().ForEach(e =>
+			{
+				var count = e.Count();
+
+				Console.WriteLine($"Artist: {e.Key}");
+				Console.WriteLine($"\tAlbums: {count}");
+			});
 		}
 	}
 }
